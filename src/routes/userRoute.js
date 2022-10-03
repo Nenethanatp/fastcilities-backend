f7b116e4b4d239fab52314fccfe938f06d41d305
Thanatporn Lawsirirattana <nthalaw21@gmail.com>
2022-10-04 02:38:15 +0700

    feat: finish login register

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
 const userController = require('../controllers/userController');
-const authenticate = require('../middlewares/authenticate');
 
 const router = express.Router();
 
-router.get('/', authenticate, userController.getUser);
+router.get('/', userController.getUser);
 
 module.exports = router;
